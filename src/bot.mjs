6817842ab52355a20b940bcc3937e7d48281de6f
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-11-17 04:22:45 +0500

    Update

--- src/bot.mjs
+++ src/bot.mjs
@@ -13,7 +13,7 @@ import {conversations, createConversation} from "@grammyjs/conversations";
  * @typedef {import("@grammyjs/conversations").Conversation} Conversation
  * @typedef {import("@grammyjs/conversations").ConversationFlavor} ConversationFlavor
  *
- * @typedef {{registered: Date} & Chat} SessionData
+ * @typedef {{registered: Date, profile: Record<string, string>} & Chat} SessionData
  * @typedef {Context & ConversationFlavor & SessionFlavor<SessionData>} BotContext
  */
 
@@ -30,6 +30,17 @@ export const {
     TELEGRAM_SECRET_TOKEN: secretToken = String(token).split(":").pop()
 
 } = process.env;
+
+const categories = {
+    "СМИ": "media",
+    "Риелтор": "realtor",
+    "Блогер/Инфлюенсер": "blogger",
+};
+
+const transports = {
+    "На трансфере": "transfer",
+    "На личном авто": "car",
+}
 
 // Default grammY bot instance
 export const bot = /** @type {Bot<BotContext>} */ new Bot(token);
@@ -51,7 +62,7 @@ bot.use(
 bot.use(conversations());
 
 async function registration(/** @type {Conversation<BotContext>} */ conversation, ctx) {
-
+    conversation.session.profile = {};
     await ctx.reply("* Приветствие *");
     await ctx.reply("* Запрос имени *", {
         reply_markup: Keyboard.from([[ctx.chat.first_name]]).resized().oneTime()
@@ -65,13 +76,38 @@ async function registration(/** @type {Conversation<BotContext>} */ conversation
         reply_markup: new Keyboard().requestContact("Нажмите сюда").resized().oneTime()
     });
     const contact = await conversation.waitFor(":contact");
-    const categories = ["СМИ", "Риелтор", "блоггер"];
     await ctx.reply("* Выбор категории *", {
-        reply_markup: Keyboard.from([categories]).toFlowed(1).resized().oneTime()
+        reply_markup: Keyboard.from([Object.keys(categories)]).toFlowed(1).resized().oneTime()
     });
-    const category = await conversation.form.select(categories);
-    await ctx.reply("* Подтверждение участия *");
-
+    const category_name = await conversation.form.select(Object.keys(categories));
+    const category = categories[category_name];
+    switch (category) {
+        case "media":
+        case "blogger":
+            await ctx.reply("* Информация о трансфере для СМИ/блогеров/инфлюенсеров *");
+            break;
+        case "realtor":
+            await ctx.reply("* Информация о трансфере для риелторов *");
+            break;
+    }
+    await ctx.reply("* Выбор трансфера *", {
+        reply_markup: Keyboard.from([Object.keys(transports)]).toFlowed(1).resized().oneTime()
+    });
+    const transport_name = await conversation.form.select(Object.keys(transports));
+    const transport = transports[transport_name];
+    Object.assign(conversation.session.profile, {first_name, last_name, contact, category, transport});
+    switch (transport) {
+        case "transfer":
+            await ctx.reply("* Запрос адреса *");
+            const address = await conversation.form.text();
+            await ctx.reply("* Запрос времени *");
+            const time = await conversation.form.text();
+            Object.assign(conversation.session, {address, time});
+    }
+    await ctx.reply("* Запрос дополнительных вопросов *");
+    const requests = await conversation.form.text();
+    Object.assign(conversation.session, {requests});
+    await ctx.reply("* Благодарность *");
 }
 
 bot.use(createConversation(registration, "registration"));
