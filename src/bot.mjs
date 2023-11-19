cba802b122ba64fcbab36bd84221ba293017880a
Vladislav Ponomarev <PonomareVlad@users.noreply.github.com>
2023-11-19 09:16:28 +0500

    Update

--- src/bot.mjs
+++ src/bot.mjs
@@ -37,13 +37,13 @@ const buttons = {
 
 const categories = {
     "СМИ": "media",
-    "Риелтор": "realtor",
-    "Блогер/Инфлюенсер": "blogger",
+    "Блогер": "blogger",
+    "Партнер": "partner",
 };
 
 const transports = {
-    "На трансфере": "transfer",
-    "На личном авто": "car",
+    "Личный автомобиль": "car",
+    "Трансфер": "transfer",
 }
 
 // Default grammY bot instance
@@ -74,71 +74,75 @@ async function registration(/** @type {Conversation<BotContext>} */ conversation
         await ctx.reply("* ошибка в формате *");
         await conversation.skip({drop: true});
     };
+    const {first_name, last_name} = ctx.chat;
     conversation.session.profile = {};
-    await ctx.reply("* Приветствие *");
-    await ctx.reply("* Запрос имени *", {
-        reply_markup: ctx.chat.first_name ? Keyboard.from([[ctx.chat.first_name]]).resized().oneTime() : undefined
-    });
-    const first_name = await conversation.form.text(inputErrorHandler);
-    await ctx.reply("* Запрос фамилии *", {
-        reply_markup: ctx.chat.last_name ? Keyboard.from([[ctx.chat.last_name]]).resized().oneTime() : undefined
-    });
-    const last_name = await conversation.form.text(inputErrorHandler);
-    await ctx.reply("* Запрос контактов *", {
-        reply_markup: new Keyboard().requestContact("Нажмите сюда").resized().oneTime()
+    await ctx.replyWithVideo("BAACAgIAAxkBAAIB22VZi7uv5T73cV7woY8mi1bTV87RAAKnPQACNDm4Spm0TI8uhHwDMwQ", {
+        caption: `Дорогой гость!
+
+Я твой друг и проводник по открытию Атриума ЖК Amundsen!
+
+Главного авантюриста ты знаешь. Давай знакомиться с тобой!`,
+        reply_markup: new Keyboard().requestContact("Давай").resized().oneTime()
     });
     const {
         contact: {message: {phone_number} = {}} = {}
     } = await conversation.waitFor(":contact", inputErrorHandler);
-    await ctx.reply("* Выбор категории *", {
+    await ctx.reply("Отправь мне ФИО", {
+        reply_markup: {
+            force_reply: true,
+            input_field_placeholder: [first_name, last_name].filter(Boolean).join(" ")
+        }
+    });
+    const name = await conversation.form.text(inputErrorHandler);
+    await ctx.reply(`Привет, ${name} !
+
+О приключениях Амундсена знал весь мир. Мы тоже хотим рассказать об открытии громко, поэтому пригласили журналистов, блогеров и партнеров.
+
+Выбери свою категорию участия.`, {
         reply_markup: Keyboard.from([Object.keys(categories)]).toFlowed(1).resized().oneTime()
     });
     const category_name = await conversation.form.select(Object.keys(categories), inputErrorHandler);
     const category = categories[category_name];
-    switch (category) {
-        case "media":
-        case "blogger":
-            await ctx.reply("* Информация о трансфере для СМИ/блогеров/инфлюенсеров *");
-            break;
-        case "realtor":
-            await ctx.reply("* Информация о трансфере для риелторов *");
-            break;
-    }
-    await ctx.reply("* Выбор трансфера *", {
+    await ctx.reply(`Мы подготовили программу, от которой захватит дух! 
+
+Как ты планируешь добираться до площадки: на личном автомобиле или на трансфере?`, {
         reply_markup: Keyboard.from([Object.keys(transports)]).toFlowed(1).resized().oneTime()
     });
     const transport_name = await conversation.form.select(Object.keys(transports), inputErrorHandler);
     const transport = transports[transport_name];
-    Object.assign(conversation.session.profile, {first_name, last_name, phone_number, category, transport});
+    Object.assign(conversation.session.profile, {name, phone_number, category, transport});
     switch (transport) {
+        case "car":
+            await ctx.reply(`Мы ждем тебя!
+
+ЖК Amundsen находится на улице Академика Ландау, 3.
+
+Мероприятие пройдет с 15:00 до 17:00.`);
+            break;
         case "transfer":
-            await ctx.reply("* Запрос адреса *", {
-                reply_markup: {
-                    force_reply: true,
-                    input_field_placeholder: "* Введите адрес *"
-                }
-            });
-            const address = await conversation.form.text(inputErrorHandler);
-            await ctx.reply("* Запрос времени *", {
-                reply_markup: {
-                    force_reply: true,
-                    input_field_placeholder: "* Введите время *"
-                }
-            });
-            const time = await conversation.form.text(inputErrorHandler);
-            Object.assign(conversation.session, {address, time});
+            switch (category) {
+                case "partner":
+                    await ctx.reply(`Компания Progress предоставляет партнерам трансфер.
+
+Он будет ждать тебя возле офиса продаж ЖК Amundsen в 14:30 на улице Вильгельма-де Геннина, 47.
+
+Дресс-код: smart casual.`);
+                    break;
+                case "media":
+                case "blogger":
+                    await ctx.reply(`Мы подготовили автомобиль бизнес-класса!   
+
+Напиши свой адрес и время, чтобы водитель смог тебя привезти на мероприятие. Оно пройдет с 15:00 до 17:00 на улице Академика Ландау, 3.`, {
+                        reply_markup: {force_reply: true, input_field_placeholder: "Адрес и время"}
+                    });
+                    const transfer = await conversation.form.text(inputErrorHandler);
+                    Object.assign(conversation.session, {transfer});
+                    await ctx.reply(`Я получил от тебя данные. Водитель приедет ${transfer}, а потом привезет обратно.
+
+За сутки до начала мероприятия я отправлю информацию о машине.`)
+                    break;
+            }
     }
-    await ctx.reply("* Запрос дополнительных вопросов *", {
-        reply_markup: Keyboard.from([[buttons.skip]]).resized().oneTime()
-    });
-    const requests = await conversation.form.text(inputErrorHandler);
-    if (requests.trim() && requests !== buttons.skip)
-        Object.assign(conversation.session, {requests});
-    await ctx.reply("* Благодарность *", {
-        reply_markup: {
-            remove_keyboard: true
-        }
-    });
 }
 
 bot.use(createConversation(registration, "registration"));
